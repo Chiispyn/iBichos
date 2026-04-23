a2ed62977881e381c063643d584fed19bce9ac02
Valesca0709 <valesca.torres07@gmail.com>
2026-04-23 13:44:37 -0400

    Fotos subidas a la nube de CloudinaryApi.kt

---
+++ iBichos-app/app/src/main/java/com/cetecom/ibichos/data/repository/CloudinaryModule.kt
@@ -0,0 +1,31 @@
+package com.cetecom.ibichos.data.repository
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.apply
+import kotlin.jvm.java
+
+
+object CloudinaryModule {
+
+    private const val BASE_URL = "https://api.cloudinary.com/"
+
+    val api: CloudinaryApi by lazy {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CloudinaryApi::class.java)
+    }
+}
---
+++ iBichos-app/app/src/main/java/com/cetecom/ibichos/domain/model/CloudinaryUploadResponse.kt
@@ -0,0 +1,7 @@
+package com.cetecom.ibichos.domain.model
+
+data class CloudinaryUploadResponse(
+    val secure_url: String? = null,
+    val url: String? = null,
+    val public_id: String? = null
+)
